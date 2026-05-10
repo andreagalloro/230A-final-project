1b4b230e3bcaaccd68180b3237b9f20a627b170f
andreagalloro <argg2159@gmail.com>
2026-05-09 17:12:38 -0700

    make file to merge 2023-2025 data. start aggregation analysis on post-covid data

---
+++ code/merge-data-post-covid.r
@@ -0,0 +1,38 @@
+####
+# This r module should be run in the main final-project directory by calling
+# source("code/merge-data-post-covid.r")
+# in an r session. binds the data into a single csv.
+####
+library(tidyverse)
+
+cat("Loading 2023 Data\n")
+od_data <- read_csv("data/date-hour-soo-dest-2018.csv", 
+                    col_names = c(
+                        "date", 
+                        "hour", 
+                        "origin",
+                        "destination",
+                        "ridership"
+                        )
+                    )
+
+for (i in seq(2024, 2025)) {
+    path <- str_c("data/date-hour-soo-dest-", i, ".csv", sep = "")
+    cat("Loading in and binding", i, "Data\n")
+    od_data <- bind_rows(od_data, 
+        read_csv(path, 
+                    col_names = c(
+                        "date", 
+                        "hour", 
+                        "origin",
+                        "destination",
+                        "ridership"
+                        )
+                    )
+    )
+
+}
+
+cat("Exporting to one csv\n")
+write_csv(od_data, "data/od-data-post-covid.csv")
+cat("done\n")
